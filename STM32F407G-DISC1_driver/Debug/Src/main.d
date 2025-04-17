e97157253b0efcd03de0372fb5578510820307a7
Jestin Varghese Cherian <jvcboy251@gmail.com>
2025-04-17 19:48:11 -0400

    started peripheral driver developement

---
+++ STM32F407G-DISC1_driver/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/jesti/OneDrive/Desktop/Embedded_System_Course/STM32F407G-DISC1_driver/driver/inc/stm32f407xx.h
+C:/Users/jesti/OneDrive/Desktop/Embedded_System_Course/STM32F407G-DISC1_driver/driver/inc/stm32f407xx.h:
---
+++ STM32F407G-DISC1_driver/Debug/driver/src/stm32f407xx_gpio_driver.d
@@ -0,0 +1,6 @@
+driver/src/stm32f407xx_gpio_driver.o: \
+ ../driver/src/stm32f407xx_gpio_driver.c \
+ C:/Users/jesti/OneDrive/Desktop/Embedded_System_Course/STM32F407G-DISC1_driver/driver/inc/stm32f407xx_gpio_driver.h \
+ C:/Users/jesti/OneDrive/Desktop/Embedded_System_Course/STM32F407G-DISC1_driver/driver/inc/stm32f407xx.h
+C:/Users/jesti/OneDrive/Desktop/Embedded_System_Course/STM32F407G-DISC1_driver/driver/inc/stm32f407xx_gpio_driver.h:
+C:/Users/jesti/OneDrive/Desktop/Embedded_System_Course/STM32F407G-DISC1_driver/driver/inc/stm32f407xx.h:
